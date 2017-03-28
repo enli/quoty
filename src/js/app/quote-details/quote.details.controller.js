7726aad5457cb903d52f1ad718883e764c672033
Peeyoosh Sangolekar <peeyoosh.sangolekar@gmail.com>
2017-03-28 21:02:09 +0530

    Scroll to top when viewing quote details.

--- src/js/app/quote-details/quote.details.controller.js
+++ src/js/app/quote-details/quote.details.controller.js
@@ -4,14 +4,17 @@
   angular.module('quoty.quote-details')
     .controller('QuoteDetailsController', QuoteDetailsController);
 
-  QuoteDetailsController.$inject = ['$stateParams', 'quote'];
+  QuoteDetailsController.$inject = ['$anchorScroll', '$timeout', '$stateParams', 'quote'];
 
-  function QuoteDetailsController($stateParams, quote) {
+  function QuoteDetailsController($anchorScroll, $timeout, $stateParams, quote) {
     var vm = this;
 
     vm.gradientClass = $stateParams.gradientClass;
     vm.quote = quote;
     vm.author = quote.author;
     vm.topics = quote.topics.join(', ');
+
+    // scroll to top
+    $timeout($anchorScroll);
   }
 })();
